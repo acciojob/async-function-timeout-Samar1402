af9f9821b0146bdfdeeb7d7d64fd5d79162aa288
Samarjeet Kumar <124017057+Samar1402@users.noreply.github.com>
2024-09-28 09:39:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+document.getElementById('btn').addEventListener('click', async function() {
+    const message = document.getElementById('text').value;
+    const delay = parseInt(document.getElementById('delay').value, 10) * 1000;
+
+    await displayMessage(message, delay);
+});
+
+
+async function displayMessage(message, delay) {
+    await new Promise(resolve => setTimeout(resolve, delay)); 
+    document.getElementById('output').innerText = message; 
